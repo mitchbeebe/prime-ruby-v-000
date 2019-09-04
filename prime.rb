476da94501835e82c6819e96365d98fb99b0365f
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-09-04 02:30:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 # Add  code here!
 def prime?(num)
-  (2..num / 2).each do |n|
-    return false if num % n == 0
-  end
+  if num < 2
+    return false
+  else
+    (2..num / 2).each do |n|
+      return false if num % n == 0
+    end
   true
 end
